7dfa6d8192ad0f9f47f46cfae8d970e4c774f0f3
Chottiwatt Jittprasong <mail@chottiwatt.in.th>
2022-11-21 04:32:33 +0800

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -2,8 +2,6 @@
 let i = 0;
 let text1 = "Hey! Burina!";
 let text2 = "It is our anniversary!"
-let text3 = "Know you laa, you always forgot"
-let text4 = "It is our anniversary!"
 let speed = 100;
 
 function typeWriter(text, para){
@@ -33,12 +31,6 @@ var typeInterval;
 		}
 		else if(ok == 1){
 			typeWriter(text2, "txt2");
-		}
-		else if(ok == 2){
-			typeWriter(text2, "txt3");
-		}
-		else if(ok == 3){
-			typeWriter(text2, "txt4");
 		}
 	}, 100);
 //};
